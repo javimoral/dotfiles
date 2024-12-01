c9b508d883e8335667960ecb7945ec91c123253f
javi <moraljlara@gmail.com>
2024-12-01 12:47:36 +0100

    Calling format command asyncronously

--- nvim/lua/cfg-lsp.lua
+++ nvim/lua/cfg-lsp.lua
@@ -28,7 +28,9 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  vim.keymap.set('n', '<leader>f', function()
+    vim.lsp.buf.format {async = true}
+  end, opts)
 
 end
 
