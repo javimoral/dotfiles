1ae77e99d59a3b7dfbacc86ca9f4d928f79eacc3
Javi <moraljlara@gmail.com>
2024-12-17 16:39:37 +0100

    zsh: Silent telescope bindings

--- nvim/lua/cfg-telescope.lua
+++ nvim/lua/cfg-telescope.lua
@@ -2,7 +2,7 @@
 require('telescope').setup {}
 
 -- Shortcuts
-vim.api.nvim_set_keymap("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>",  {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fg", ":lua require('telescope.builtin').live_grep()<cr>",  {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fr", ":lua require('telescope.builtin').lsp_references()<cr>",  {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fs", ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>",  {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>fg", ":lua require('telescope.builtin').live_grep()<cr>",  {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>fr", ":lua require('telescope.builtin').lsp_references()<cr>",  {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>fs", ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>", {noremap = true, silent = true})
