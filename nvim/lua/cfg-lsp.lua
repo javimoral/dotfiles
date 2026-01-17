b306270c5bc1df9786f48b9bca40c1e9d1f3b0ec
Javi <moraljlara@gmail.com>
2026-01-17 14:19:17 +0100

    nvim: Updated lsp configuration with modern API. Removed efm redundancy as ruff is already configured

--- nvim/lua/cfg-lsp.lua
+++ nvim/lua/cfg-lsp.lua
@@ -2,36 +2,34 @@
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-
+  -- Enable completion triggered by <c-x><c-o>
+  vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+  
+  -- Mappings
+  local opts = { buffer = bufnr }
+  
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = 'Go to declaration' })
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'Go to definition' })
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover documentation' })
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = 'Go to references' })
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = bufnr, desc = 'Go to implementation' })
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'Signature help' })
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { buffer = bufnr, desc = 'Add workspace folder' })
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { buffer = bufnr, desc = 'Remove workspace folder' })
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, { buffer = bufnr, desc = 'List workspace folders' })
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'Type definition' })
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = 'Rename symbol' })
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = 'Code action' })
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { buffer = bufnr, desc = 'Show line diagnostics' })
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr, desc = 'Previous diagnostic' })
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr, desc = 'Next diagnostic' })
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { buffer = bufnr, desc = 'Diagnostics to loclist' })
   vim.keymap.set('n', '<leader>f', function()
-    vim.lsp.buf.format {async = true}
-  end, opts)
-
+    vim.lsp.buf.format { async = true }
+  end, { buffer = bufnr, desc = 'Format buffer' })
 end
 
 -- nvim-cmp supports additional completion capabilities
@@ -39,29 +37,16 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Rust
-vim.lsp.config('rust_analyzer', {capabilities = capabilities, on_attach = on_attach})
+vim.lsp.config('rust_analyzer', { capabilities = capabilities, on_attach = on_attach })
 --vim.lsp.enable('rust_analyzer')
 
 -- Python
-vim.lsp.config('jedi_language_server', {capabilities = capabilities, on_attach = on_attach})
+vim.lsp.config('jedi_language_server', { capabilities = capabilities, on_attach = on_attach })
 vim.lsp.enable('jedi_language_server')
-vim.lsp.config('ruff', {capabilities = capabilities, on_attach = on_attach})
+
+vim.lsp.config('ruff', { capabilities = capabilities, on_attach = on_attach })
 vim.lsp.enable('ruff')
-vim.lsp.config('ty', {capabilities = capabilities, on_attach = on_attach})
+
+vim.lsp.config('ty', { capabilities = capabilities, on_attach = on_attach })
 vim.lsp.enable('ty')
 
--- General Purpose Lang Server, used to linting & formatting
--- https://github.com/mattn/efm-langserver
-vim.lsp.config('efm', {
-  init_options = {documentFormatting = true},
-  settings = {
-    rootMarkers = {'.git/'},
-    languages = {
-      python = {
-	{formatCommand = 'ruff check --quiet --select I --fix -', formatStdin = true},
-      }
-    },
-    filetypes = { 'python' }
-  }
-})
-vim.lsp.enable('efm')
